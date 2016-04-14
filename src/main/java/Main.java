51c32ef93169bf2b99f1fa0cb0b7c49b6968c652
eivwik <eivind.wikheim@kantega.no>
2016-04-14 16:08:28 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,118 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        loadProperties();
+        String url = System.getProperty("team.url");
+        try {
+            sendGet(url);
+        } catch (Exception e) {
+            System.err.println("Something went wrong: " + e);
+        }
+    }
+
+
+    private static void loadProperties() {
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            String filename = "team.properties";
+            input = Main.class.getClassLoader().getResourceAsStream(filename);
+            if (input == null) {
+                System.out.println("Sorry, unable to find " + filename);
+                return;
+            }
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            List<String> list = Arrays.asList(prop.getProperty("team.name").split(","));
+            String name = list.get((new Random()).nextInt(list.size()));
+            System.setProperty("team.name", (name != null)? name : "n/a" );
+            System.setProperty("team.url", prop.getProperty("team.url"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+
+    // HTTP GET request
+    private static void sendGet(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", "DockerClient");
+        con.setRequestProperty("name", System.getProperty("team.name"));
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+    }
+
+    // HTTP POST request
+    @SuppressWarnings("unused")
+    private static void sendPost(String url) throws Exception {
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add request header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", "DockerClient");
+        con.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+
+        String urlParameters = "sn=C02G8416DRJM&cn=&locale=&caller=&num=12345";
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+        //print result
+        System.out.println(response.toString());
+    }
+}
